7ce72897cd7891b48a80342bb5ade88b5b232cc8
sushiz <sushiz@users.noreply.github.com>
2020-08-16 19:02:05 +0800

    Fixed a bug where the app crashes if user grants location permission for the first time

--- app/src/main/java/com/example/memorableplaces/MapsActivity.java
+++ app/src/main/java/com/example/memorableplaces/MapsActivity.java
@@ -47,7 +47,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public void onMapReady(final GoogleMap googleMap) {
+        Intent intent = getIntent();
+        int itemClicked = intent.getIntExtra("itemNumber",-1);
         mMap = googleMap;
+        LatLng userLocation;
 
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         locationListener = new LocationListener() {
@@ -73,51 +76,48 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         };
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,5000,1,locationListener);
-            lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
             mMap.setMyLocationEnabled(true);
+            lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
             if(lastKnownLocation == null){
+                Log.i("Info:","GPS provider fail");
                 lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
                 if(lastKnownLocation == null){
+                    Log.i("Info:","Network provider fail");
                     lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
                     if(lastKnownLocation == null){
+                        Log.i("Info:","Passive provider fail, setting default");
                         lastKnownLocation = new Location("DEFAULT");
                         lastKnownLocation.setLatitude(25.033964);
                         lastKnownLocation.setLongitude(121.564468);
-
                     }
                 }
-            }else {
-                Log.i("Last Known Location", lastKnownLocation.toString());
-                LatLng userLocation = new LatLng(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude());
+            }
+            Log.i("Last Known Location", lastKnownLocation.toString());
+            userLocation = new LatLng(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude());
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 12));
+            Log.i("Item clicked",String.valueOf(itemClicked));
+            LatLng pos;
+            String address;
+            if(itemClicked != -1){
+                pos = MainActivity.myPlaces.get(itemClicked);
+                address = MainActivity.myAddresses.get(itemClicked);
+                Log.i("pos",pos.toString());
+                Log.i("address",address);
+                mMap.addMarker(new MarkerOptions().position(pos).title("Saved Location").snippet(address));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(pos, 12));
+            }
+            else{
+                if(MainActivity.myAddresses.size()>0){
+                    for(int i=0;i<MainActivity.myAddresses.size();i++){
+                        pos = MainActivity.myPlaces.get(i);
+                        address = MainActivity.myAddresses.get(i);
+                        mMap.addMarker(new MarkerOptions().position(pos).title("Saved Location").snippet(address));
+                    }
+                }
                 mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 12));
             }
         }else{
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},0);
-        }
-
-        Intent intent = getIntent();
-        int itemClicked = intent.getIntExtra("itemNumber",-1);
-        Log.i("Item clicked",String.valueOf(itemClicked));
-        LatLng pos;
-        String address;
-        if(itemClicked != -1){
-            pos = MainActivity.myPlaces.get(itemClicked);
-            address = MainActivity.myAddresses.get(itemClicked);
-            Log.i("pos",pos.toString());
-            Log.i("address",address);
-            mMap.addMarker(new MarkerOptions().position(pos).title("Saved Location").snippet(address));
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(pos, 12));
-        }
-        else{
-            if(MainActivity.myAddresses.size()>0){
-                for(int i=0;i<MainActivity.myAddresses.size();i++){
-                    pos = MainActivity.myPlaces.get(i);
-                    address = MainActivity.myAddresses.get(i);
-                    mMap.addMarker(new MarkerOptions().position(pos).title("Saved Location").snippet(address));
-                }
-            }
-            pos = new LatLng(lastKnownLocation.getLatitude(),lastKnownLocation.getLongitude());
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(pos, 10));
         }
 
         mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
@@ -156,8 +156,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
                 if(ContextCompat.checkSelfPermission(getApplicationContext(),Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
                     locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,5000,1,locationListener);
-                    lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
                     mMap.setMyLocationEnabled(true);
+                    lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
                     if(lastKnownLocation == null){
                         Log.i("Info:","GPS provider fail");
                         lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
@@ -165,11 +165,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                             Log.i("Info:","Network provider fail");
                             lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
                             if(lastKnownLocation == null){
-                                Log.i("Info:","Network provider fail, setting default");
+                                Log.i("Info:","Passive provider fail, setting default");
                                 lastKnownLocation = new Location("DEFAULT");
                                 lastKnownLocation.setLatitude(25.033964);
                                 lastKnownLocation.setLongitude(121.564468);
-
                             }
                         }
                     }
